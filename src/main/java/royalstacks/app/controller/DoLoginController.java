87ed54dd76893cb75fb9ccd54e6f111697307b33
Ymke <ymkedevink@gmail.com>
2020-05-22 11:38:47 +0200

    Index html en controller verwijdert, omdat deze niks deden. Unittest geschreven voor de HomepageController (integratietest n.v.t., functionele test > is dat nodig???)

---
+++ src/main/java/royalstacks/app/controller/DoLoginController.java
@@ -0,0 +1,48 @@
+package royalstacks.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import royalstacks.app.model.Customer;
+import royalstacks.app.model.Employee;
+import royalstacks.app.service.LogInService;
+
+@Controller
+public class DoLoginController {
+
+    @Autowired
+    private LogInService loginService;
+    private String username;
+    private String password;
+
+    @PostMapping("/doLogin")
+    @ResponseBody
+    public ModelAndView doLoginHandler(@RequestParam String inputUsername, String inputPassword){
+        //Check if username exists in database
+        if (loginService.findByUsername(inputUsername) == null){
+            return new ModelAndView("homepage");
+            //TODO nette melding naar gebruiker dat ingevoerde gebruikersnaam niet bestaat
+        }
+
+        //Check if password of user matches entered value
+        if (!loginService.findByUsername(inputUsername).getPassword().equals(inputPassword)){
+            return new ModelAndView("homepage");
+            //TODO nette melding naar gebruiker dat ingevoerde wachtwoord niet hoort bij de user
+        }
+
+        //redirect user to next page
+        if (loginService.findByUsername(inputUsername) instanceof Employee){
+            Employee employee = (Employee)loginService.findByUsername(inputUsername);
+            ModelAndView mav = new ModelAndView("headprivateoverview");
+            return mav.addObject(employee);
+        } else if (loginService.findByUsername(inputUsername) instanceof Customer){
+            Customer customer = (Customer)loginService.findByUsername(inputUsername);
+            ModelAndView mav = new ModelAndView("myaccounts");
+            return mav.addObject(customer);
+        } else {
+            return new ModelAndView("homepage");
+        }
+
+    }
+}
---
+++ src/test/java/royalstacks/app/controller/HomepageControllerTest.java
@@ -0,0 +1,24 @@
+package royalstacks.app.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomepageControllerTest {
+
+    @Test
+    void startHandlerTest() {
+        //ARRANGE
+        HomepageController homepageController = new HomepageController();
+
+        //ACT
+        ModelAndView result = homepageController.startHandler();
+
+        //ASSERT
+        String expected = "homepage";
+        assertEquals(expected,result.getViewName());
+
+    }
+}
