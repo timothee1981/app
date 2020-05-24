580c9a542959fa34c187657ee1e9c52a8063ae6a
Tom Janssen <t.janssen13788@gmail.com>
2020-05-24 16:50:19 +0200

    naam van test aangepast

---
+++ src/test/java/royalstacks/app/backingBean/OpenAccountControllerBackingBeanTest.java
@@ -0,0 +1,12 @@
+package royalstacks.app.backingBean;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenAccountControllerBackingBeanTest {
+
+    @Test
+    void generateAccountNumber() {
+    }
+}
