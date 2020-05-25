a6f56eb4b4b901e0cf0491fc801e5dc70ac1c5e0
Ymke <ymkedevink@gmail.com>
2020-05-25 15:23:56 +0200

    Errorhandling toegevoegd voor inloggen.

--- src/main/java/royalstacks/app/controller/DoLoginController.java
+++ src/main/java/royalstacks/app/controller/DoLoginController.java
@@ -17,41 +17,46 @@ public class DoLoginController {
 
     @PostMapping("/doLogin")
     @ResponseBody
-    public ModelAndView doLoginHandler(@RequestParam String inputUsername, String inputPassword){
+    public ModelAndView doLoginHandler(@RequestParam String inputUsername, String inputPassword) {
 
         //Check if username and password exist and have a value
         boolean usernameHasValue = inputUsernameHasValue(inputUsername);
         boolean passwordHasValue = inputpasswordHasValue(inputPassword);
-        if( ! (usernameHasValue && passwordHasValue ) ){
-            return new ModelAndView("homepage");
-            //TODO nette melding naar gebruiker dat beide velden gevuld moeten worden
+        if (!(usernameHasValue && passwordHasValue)) {
+            ModelAndView mav = new ModelAndView("homepage");
+            mav.addObject("username_error", "username is required");
+            mav.addObject("password_error", "password is required");
+            return mav;
         }
 
         //Check if username exists in database
         User user = loginService.findByUsername(inputUsername);
-        if (user == null){
-            return new ModelAndView("homepage");
-            //TODO nette melding naar gebruiker dat ingevoerde gebruikersnaam niet bestaat
+        if (user == null) {
+            ModelAndView mav = new ModelAndView("homepage");
+            mav.addObject("username_error", "username does not exist, please enter a valid username");
+            return mav;
         }
 
         //Check if password of user matches entered value
-        if (!user.getPassword().equals(inputPassword)){
-            return new ModelAndView("homepage");
-            //TODO nette melding naar gebruiker dat ingevoerde wachtwoord niet hoort bij de user
+        if (!user.getPassword().equals(inputPassword)) {
+            ModelAndView mav = new ModelAndView("homepage");
+            mav.addObject("password_error", "password is not correct, please enter a valid password");
+            return mav;
         }
 
         //redirect user to next page
-        if (user instanceof Employee){
+        if (user instanceof Employee) {
             Employee employee = (Employee) user;
             ModelAndView mav = new ModelAndView("headprivateoverview");
             return mav.addObject(employee);
-        } else if (user instanceof Customer){
+        } else if (user instanceof Customer) {
             Customer customer = (Customer) user;
             ModelAndView mav = new ModelAndView("myaccounts");
             return mav.addObject(customer);
         } else {
-            return new ModelAndView("homepage");
-            //TODO nette melding naar gebruiker dat hij geen employee of customer is
+            ModelAndView mav = new ModelAndView("homepage");
+            mav.addObject("user_error", "you are neither a customer or employee of Royal Stacks Bank");
+            return mav;
         }
 
     }
@@ -60,7 +65,7 @@ public class DoLoginController {
     private boolean inputUsernameHasValue(String inputUsername) {
         boolean inputUserNameHasValue = true;
 
-        if(inputUsername.trim().equals("")){
+        if (inputUsername.trim().equals("")) {
             inputUserNameHasValue = false;
         }
         return inputUserNameHasValue;
@@ -69,7 +74,7 @@ public class DoLoginController {
     private boolean inputpasswordHasValue(String inputPassword) {
         boolean inputPasswordHasValue = true;
 
-        if(inputPassword.trim().equals("")){
+        if (inputPassword.trim().equals("")) {
             inputPasswordHasValue = false;
         }
         return inputPasswordHasValue;
