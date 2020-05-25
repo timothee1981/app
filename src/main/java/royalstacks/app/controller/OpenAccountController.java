449a427fe122fae4343167526811d820cd6a0ea1
Timothee <timotheebusch@hotmail.com>
2020-05-25 16:52:35 +0200

    open account controller schoon gemaakt, span toeggevoegd aan html

--- src/main/java/royalstacks/app/controller/OpenAccountController.java
+++ src/main/java/royalstacks/app/controller/OpenAccountController.java
@@ -20,55 +20,66 @@ public class OpenAccountController {
     }
 
     @PostMapping("/openaccount")
-    public ModelAndView createBusinessAccountHandler(@ModelAttribute ("account") OpenAccountBackingBean bb) {
+    public ModelAndView createAccountHandler(@ModelAttribute ("account") OpenAccountBackingBean bb) {
 
         ModelAndView mav = new ModelAndView("createAccountConfirmation");
         ModelAndView mav2 = new ModelAndView("openaccount");
-        boolean save = false;
+        if (bb.getAccountType().equals("business"))
+            return createBusinessAccount(bb,mav,mav2);
+        else
+            return createPrivateAccount(bb,mav);
+    }
 
-
-        if(bb.getAccountType().equals("business")) {
-
-            bb.setAccountNumber(accountService.generateAccountNumber());
-            BusinessAccount businessAccount = bb.businessAccount();
-            //checken als alle velden valid zijn
-                //check company name
-            if(!businessAccount.isCompanyNameFormatValid()) {
-                mav2.addObject("companyName","invalid name");
-                save = false;
-            } //check kvknumber
-            if(!businessAccount.isCompanyNameFormatValid()) { //if false save false, full attribuut value
-                mav2.addObject("kvkNumber","number needs to have 8 digits");
-                save = false;
-            }
-
-
-            //check Vat number
-                 //if false save false, full attribuut value
-
-
-            //if save boolean is false dan ga na openaccounview
-
-            // else save in db en ga na confirmation page (of later confirmation popup)
-            accountService.saveAccount(businessAccount);
-
-        }
-        else if (bb.getAccountType().equals("private")) {
-            bb.setAccountNumber(accountService.generateAccountNumber());
-            PrivateAccount privateAccount = bb.privateAccount();
-            accountService.saveAccount(privateAccount);
-
-        }
-
+    //Do private account method
+    private ModelAndView createPrivateAccount(OpenAccountBackingBean bb, ModelAndView mav) {
+        bb.setAccountNumber(accountService.generateAccountNumber());
+        PrivateAccount privateAccount = bb.privateAccount();
+        accountService.saveAccount(privateAccount);
         mav.addObject("account", bb);
-
-
-
-
-        // go To  confirmation page: message+account number, ToMyAccountButton , ToAccountDetails
         return mav;
+    }
+
+    //do business account view
+    private ModelAndView createBusinessAccount(OpenAccountBackingBean bb, ModelAndView mav, ModelAndView mav2) {
+        bb.setAccountNumber(accountService.generateAccountNumber());
+        BusinessAccount businessAccount = bb.businessAccount();
+        //checken als alle velden valid zijn
+        if(isAllInputValid(businessAccount, mav2)) {
+            accountService.saveAccount(businessAccount);
+            mav.addObject("account", bb);
+            return mav;
+        }
+        else
+            populateFields(businessAccount,mav2);
+            return mav2;
+
+    }
 
 
+    //populate the field of the view when
+    private void populateFields(BusinessAccount businessAccount, ModelAndView mav2) {
+
+        mav2.addObject("companyName",businessAccount.getCompanyName());
+        mav2.addObject("kvkNumber",businessAccount.getKvkNumber());
+        mav2.addObject("vatNumber", businessAccount.getVatNumber());
+    }
+
+    //check if inputs for business account are valid
+
+    private boolean isAllInputValid(BusinessAccount businessAccount, ModelAndView mav) {
+        boolean save = true;
+        if (!businessAccount.isCompanyNameFormatValid()) {
+            mav.addObject("companyName_error", "invalid name");
+            save = false;
+        } //check kvknumber
+        if (!businessAccount.isKvkNameFormatValid()) { //if false save false, full attribuut value
+            mav.addObject("kvkNumber_error", "number needs to have 8 digits");
+            save = false;
+        }
+        //check Vat number
+
+        return save;
+    }
 
 
 
@@ -87,6 +98,6 @@ public class OpenAccountController {
         }
     }*/
 
-    }
+
 
 }
