476f247355f9a714bb9299356053f20ef57e788b
Timothee <timotheebusch@hotmail.com>
2020-06-01 12:53:10 +0200

    checks on all fields with jvs

--- src/main/resources/static/js/accountValidation.js
+++ src/main/resources/static/js/accountValidation.js
@@ -46,6 +46,7 @@ kvknumber.addEventListener('input',function(){
 const vatnumber = document.getElementById("vatNumber");
 vatnumber.addEventListener('input', function () {
     let vatnumberInput = vatnumber.value;
+    let VATchek = window.location.pathname + `/v_check?VAT=${vatnumberInput}`;
     const re = /^[nN][lL][0-9]{9}[bB][0-9]{2}$/;
 
     if(re.test(vatnumberInput)) {
@@ -53,81 +54,88 @@ vatnumber.addEventListener('input', function () {
         vatnumber.classList.add("isValid");
         vatnumber.classList.remove("isInvalid");
 
-    } else {
+    }else  {
         setVatNumberNotCorrect("Vat number not valid");
         vatnumber.classList.add("isInvalid");
-        vatnumber.classList.remove("isValid");
+        vatnumber.classList.remove("isValid")
+
     }
+
+    //STILL TO BE DONE SO THAT IT CAN BE CHECKED ON THE 11 PROOF!!!!!!!!!!!!!!!!!!!!!!!!!
+   /*fetch(VATchek)
+       .then((response) => {
+           if(!response){
+               throw new Error("Response Error")
+
+           }
+           return   response.json();
+       })
+       .then((boolean) => {
+           if (boolean === true) {
+               hideVatNumberNotCorrect();
+               vatnumber.classList.add("isValid");
+               vatnumber.classList.remove("isInvalid")
+           } else {
+               setVatNumberNotCorrect("Vat number not valid");
+               vatnumber.classList.add("isInvalid");
+               vatnumber.classList.remove("isValid")
+           }
+       })
+       .catch((error) => {
+           console.log(error);
+       })*/
+
+
 });
 
 
-
-
-
 /* check which button is checked */
-
-    //CHECK WHICH BUTTON IS CHECKED
-
 const form = document.getElementById("form");
 
-form.addEventListener('mouseout',function () {
-    if (document.getElementById("business").checked){
-        document.getElementById("businessFields").style.visibility = "visible";
-        if(companyname.classList.contains("isValid")&&
-            kvknumber.classList.contains("isValid")/*&&
-            vatnumber.classList.contains("isValid")*/&&
-            sector.value !== ""){
-            document.getElementById("submitButton").disabled = false;
-        }else{
-            document.getElementById("submitButton").disabled = true;
-        }
-
-    } else {
+form.addEventListener('mousemove',function () {
+    if (document.getElementById("business").checked) {
+        showBusinessFields();
+    }else {
         hideBusinessFields();
     }
 });
 
 
 
-/* hide or show fields according to which button is checked */
 
 
+/* SHOW BUSINESS FIELD METHOD */
+
+function showBusinessFields(){
 
 
-function showBusinessFields() {
-
-    /*form.addEventListener('keyup ',function () {
-        if (companyname.classList.contains("isValid") &&
-            kvknumber.classList.contains("isValid")/!*&&
-            vatnumber.classList.contains("isValid")*!/&&
-            sector.value !== "") {
-            document.getElementById("submitButton").disabled = false;
-        } else {
-            document.getElementById("submitButton").disabled = true;
-        }
-
-    });
-*/
-   /* document.getElementById("submitButton").disabled = true;
     document.getElementById("businessFields").style.visibility = "visible";
+    document.getElementById("submitButton").disabled = true;
+
+
+        // CHECK FIELD ARE VALID
+
+    if (companyname.classList.contains("isValid") &&
+        kvknumber.classList.contains("isValid") &&
+        vatnumber.classList.contains("isValid") &&
+        sector.value !== "") {
+        document.getElementById("submitButton").disabled = false;
+    } else {
+        document.getElementById("submitButton").disabled = true;
+    }
+
+
+
     document.getElementById("companyName").required = true;
     document.getElementById("kvkNumber").required = true;
     document.getElementById("vatNumber").required = true;
-    document.getElementById("sector").required = true;*/
-
-
-   /* const companyId = document.getElementById("companyName");
-    companyId.addEventListener('input',function () {
-        if(companyname.classList.contains("isValid")){
-            document.getElementById("submitButton").disabled = false;
-        }
-    });*/
-
-
-
+    document.getElementById("sector").required = true;
 
 
 }
+
+
+/*HIDE BUSINESS FIELD METHOD*/
 
 function hideBusinessFields() {
     document.getElementById("submitButton").disabled = false;
@@ -140,22 +148,6 @@ function hideBusinessFields() {
 
 }
 
-
-/*const form = document.getElementById("form");
-function change() {
-    document.getElementById("submitButton").disabled = true;
-    form.addEventListener('keyup ', function () {
-        if (companyname.classList.contains("isValid") &&
-            kvknumber.classList.contains("isValid") &&
-            vatnumber.classList.contains("isValid") &&
-            sector.value !== "") {
-            document.getElementById("submitButton").disabled = false;
-        } else {
-            document.getElementById("submitButton").disabled = true;
-        }
-
-    });
-}*/
 
 
 
