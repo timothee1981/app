7869c294de620c41883a118480b71c5e28fc289f
Timothee <timotheebusch@hotmail.com>
2020-06-02 17:01:22 +0200

    begonnen aan simple view en implementatie van Accountdetails controller

--- src/main/java/royalstacks/app/controller/AccountDetails.java
+++ src/main/java/royalstacks/app/controller/AccountDetails.java
@@ -1,7 +1,31 @@
 package royalstacks.app.controller;
 
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import royalstacks.app.model.Account;
+import royalstacks.app.model.PrivateAccount;
+import royalstacks.app.service.AccountService;
 
 @Controller
 public class AccountDetails {
+    AccountService accountService;
+
+    public AccountDetails() {
+    }
+
+    @GetMapping("/accountdetails")
+    public ModelAndView accountDetailsHandler(){
+        //Account object, take it from the database
+        Account account = new PrivateAccount();
+
+        //list from account holders
+        //if account obj is private then give to a field accounttype: private, else accountype business
+
+        ModelAndView mav = new ModelAndView("/accountdetails");
+        mav.addObject("account",account);
+        return mav;
+    }
 }
---
+++ src/test/java/royalstacks/app/controller/AccountDetailsTest.java
@@ -0,0 +1,7 @@
+package royalstacks.app.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDetailsTest {
+
+}
