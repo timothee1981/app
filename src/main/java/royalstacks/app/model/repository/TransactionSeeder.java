a720fe9af5ff518543f47e36b6da33d4a4616889
W.W.W. Wong <wesley.wong@hva.nl>
2020-06-04 14:31:37 +0200

    executeTransaction slaat transactie op in MongoDB

--- src/main/java/royalstacks/app/model/repository/TransactionSeeder.java
+++ src/main/java/royalstacks/app/model/repository/TransactionSeeder.java
@@ -28,30 +28,5 @@ public class TransactionSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        LocalDateTime timestamp = LocalDateTime.now();
-        double amount = 129.95;
-        Account accountFrom = new PrivateAccount(accountService.createNewIban(), 1500);
-        Account accountTo = new PrivateAccount(accountService.createNewIban(), 0);
-        String description = "First transaction test";
-        Transaction transaction1 = new Transaction(1, 2, amount, description, timestamp);
-
-        LocalDateTime timestamp2 = LocalDateTime.now().minusMonths(1);
-        double amount2 = 99;
-        Account accountFrom2 = new PrivateAccount(accountService.createNewIban(), 999);
-        Account accountTo2 = new PrivateAccount(accountService.createNewIban(), 99);
-        String description2 = "Second transaction test";
-        Transaction transaction2 = new Transaction(3, 4, amount2, description2, timestamp2);
-
-        //drop all transactions
-        transactionRepository.deleteAll();
-
-        //add transactions to db
-        List<Transaction> transactions = new ArrayList<>();
-        transactions.add(transaction1);
-        transactions.add(transaction2);
-        transactionRepository.saveAll(transactions);
-
-
-
     }
 }
