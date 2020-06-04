757992343b7ee4dd446099cda44957419ad27a0c
Suzanne Knauff <suzanneknauff@gmail.com>
2020-06-04 17:25:11 +0200

    start postmapping addaccountholdercontroller met check op 5 digits

--- src/main/java/royalstacks/app/controller/AddAccountHolderController.java
+++ src/main/java/royalstacks/app/controller/AddAccountHolderController.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import royalstacks.app.backingBean.AccountHolderInviteBackingBean;
 import royalstacks.app.model.Account;
-import royalstacks.app.model.Customer;
+import royalstacks.app.model.User;
 import royalstacks.app.service.AccountService;
 import royalstacks.app.service.UserService;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -37,6 +37,27 @@ public class AddAccountHolderController {
         }
         return mav;
     }
+
+    @PostMapping("/addaccountholder")
+    public ModelAndView addAccountHolderHandler(@ModelAttribute AccountHolderInviteBackingBean ibb,
+                                                @SessionAttribute("userid") int userId,
+                                                Model model) {
+        ModelAndView mav = new ModelAndView("addaccountholder");
+
+        //haal user op uit DB met behulp van backing bean
+        Optional<User> OptionalUser = userService.findByUsername(ibb.getInviteeUsername());
+        //check of user bestaat én een employee is (geen customer)
+
+
+        if (userIsPresent && userIsCustomer && ibb.getVerificationCode().matches("\\d{5}")){
+
+        }
+    }
+
+/*    public boolean isVerificationCodeValid(){
+        this.verificationCode = this.verificationCode.trim();
+        return this.verificationCode.matches("\\d{5}");
+    }*/
 
 
 }
