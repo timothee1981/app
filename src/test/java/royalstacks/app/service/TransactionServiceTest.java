0358e0425e073a6c9b42a0703322c8d45b7a7d59
Timothee <timotheebusch@hotmail.com>
2020-06-04 18:16:42 +0200

    begonnen aan test voor transaction

--- src/test/java/royalstacks/app/service/TransactionServiceTest.java
+++ src/test/java/royalstacks/app/service/TransactionServiceTest.java
@@ -1,12 +1,47 @@
 package royalstacks.app.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import royalstacks.app.model.repository.TransactionRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TransactionServiceTest {
+@RunWith(SpringRunner.class)
+public class TransactionServiceTest {
+
+
+
+    @TestConfiguration
+    static class TransactionServiceTestContextConfiguration{
+
+
+        @Bean
+        public TransactionService transactionService(){
+            return new TransactionService();
+        }
+    }
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @Test
     void getTenLastTransaction() {
+
+        //Create a list of transactions
+
+        //save it into nosqldb
+
+        //retrieve list
+
+        //check first size of list, sould not be greater than 10
+
+        //check that we got the last ten transaction object belonging to account
     }
 }
