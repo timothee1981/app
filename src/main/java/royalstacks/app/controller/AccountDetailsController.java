36e2a9acb27511f659b3f0c8eeb078d70179ad2d
Suzanne Knauff <suzanneknauff@gmail.com>
2020-06-04 14:20:11 +0200

    voegt postmapping toe in accountdetailscontroller

--- src/main/java/royalstacks/app/controller/AccountDetailsController.java
+++ src/main/java/royalstacks/app/controller/AccountDetailsController.java
@@ -23,7 +23,7 @@ public class AccountDetailsController {
     }
 
     @GetMapping("/accountdetails")
-    public ModelAndView accountDetailsHandler( @SessionAttribute("userid") int userId, @RequestParam(value = "accountNumber",required = false) String accountNumber) {
+    public ModelAndView accountDetailsHandler(@SessionAttribute("userid") int userId, @RequestParam(value = "accountNumber", required = false) String accountNumber) {
 
         ModelAndView mav = new ModelAndView("/accountdetails");
 
@@ -32,12 +32,12 @@ public class AccountDetailsController {
 
         //TODO DONT YOU FORGET ABOUT DATE AND TIME: BAAACCCKING BEAAAAANNN
 
-         Account myAccount = getAccountFromAccountNumber(accountNumber);
+        Account myAccount = getAccountFromAccountNumber(accountNumber);
 
-         populatefields(mav,myAccount);
+        populatefields(mav, myAccount);
 
         //TODO: get transactions corresponding to this account from nosql DB en show only ten last transaction
-        mav.addObject("accounts",myAccounts);
+        mav.addObject("accounts", myAccounts);
 
 
         return mav;
@@ -45,78 +45,74 @@ public class AccountDetailsController {
 
     }
 
-/*    @PostMapping("/addaccountholder")
-    public ModelAndView addAccountHolderHandler(@RequestParam String accountNumber, Model model)*/
-
-    @PostMapping("/doLogin")
-    public ModelAndView doLoginHandler(@RequestParam String inputUsername, String inputPassword, Model model){
-
-        //Check if username has a value
-        boolean usernameHasValue = inputUsernameHasValue(inputUsername);
-        if (!usernameHasValue) {
-            ModelAndView mav = new ModelAndView("homepage");
-            mav.addObject("username_error", "Username is required");
-            return mav;
+    @PostMapping("/addaccountholder")
+    //ModelAttribute of RequestParam gebruiken?
+    public ModelAndView addAccountHolderHandler(@ModelAttribute String accountNumber, Model model) {
+        ModelAndView mav = new ModelAndView("/addaccountholder");
+        Account accountForInvite = getAccountFromAccountNumber(accountNumber);
+        if (accountForInvite != null) {
+            mav.addObject("accountNumber", accountForInvite.getAccountNumber());
         }
-
-
-    //METHODE DIE VELDEN VULLEN
-    private void populatefields(ModelAndView mav,Account myAccount) {
-
-        //check als My account heeft waarde
-        if(myAccount != null){
-            List<Customer> accountholders = getAccountHolders(myAccount);
-            mav.addObject("accountNumber",myAccount.getAccountNumber());
-            mav.addObject("balance", myAccount.getBalance());
-            mav.addObject("list",accountholders);
-        }
-    }
-
-    //METHODE DIE LIJST MET ACCOUNT HOLDERS TERUG GEEFT
-
-    private List<Customer> getAccountHolders(Account myAccount) {
-        return new ArrayList<>(myAccount.getAccountHolders());
-
-    }
-
-    //METHODE DIE JUISTE ACCOUNT TERUG GEEFT
-
-    private Account getAccountFromAccountNumber(String accountNumber) {
-        Account myAccount = null;
-        Optional<Account> account = accountService.getAccountByAccountNumber(accountNumber);
-        if(account.isPresent()){
-            myAccount = account.get();
-        }
-
-        return myAccount;
-    }
-
-    //METHODE DIE DE DROPDOWN VULT MET ACCOUNTS DIE HOREN BIJ HET GEBRUIKER
-
-    private List<Account> getAccountsFromUserId(int userId) {
-        List<Account> myAccounts = new ArrayList<>();
-        Customer customer = (Customer) userService.findByUserId(userId);
-        Iterator<Account> accounts = customer.getAccount().iterator();
-        while(accounts.hasNext()){
-            myAccounts.add(accounts.next());
-        }
-
-        return myAccounts;
+        return mav;
     }
 
 
-    //METHODE DIE ACCOUNT TYPE TERUG GEEFT, MAAR DIE IS WAARSCHIJNLIJK NIET NODIG ALS BACKING BEAN WERKT LETS SEE
+        //METHODE DIE VELDEN VULLEN
+        private void populatefields (ModelAndView mav, Account myAccount){
 
+            //check als My account heeft waarde
+            if (myAccount != null) {
+                List<Customer> accountholders = getAccountHolders(myAccount);
+                mav.addObject("accountNumber", myAccount.getAccountNumber());
+                mav.addObject("balance", myAccount.getBalance());
+                mav.addObject("list", accountholders);
+            }
+        }
 
-    private String getAccountType (Account account){
-        String accountType = null;
-        if (account instanceof BusinessAccount)
-            return accountType = "Business Account";
-        else
-            return accountType = "Private Account";
+        //METHODE DIE LIJST MET ACCOUNT HOLDERS TERUG GEEFT
+
+        private List<Customer> getAccountHolders (Account myAccount){
+            return new ArrayList<>(myAccount.getAccountHolders());
+
+        }
+
+        //METHODE DIE JUISTE ACCOUNT TERUG GEEFT
+
+        private Account getAccountFromAccountNumber (String accountNumber){
+            Account myAccount = null;
+            Optional<Account> account = accountService.getAccountByAccountNumber(accountNumber);
+            if (account.isPresent()) {
+                myAccount = account.get();
+            }
+
+            return myAccount;
+        }
+
+        //METHODE DIE DE DROPDOWN VULT MET ACCOUNTS DIE HOREN BIJ HET GEBRUIKER
+
+        private List<Account> getAccountsFromUserId ( int userId){
+            List<Account> myAccounts = new ArrayList<>();
+            Customer customer = (Customer) userService.findByUserId(userId);
+            Iterator<Account> accounts = customer.getAccount().iterator();
+            while (accounts.hasNext()) {
+                myAccounts.add(accounts.next());
+            }
+
+            return myAccounts;
         }
 
 
+        //METHODE DIE ACCOUNT TYPE TERUG GEEFT, MAAR DIE IS WAARSCHIJNLIJK NIET NODIG ALS BACKING BEAN WERKT LETS SEE
 
-}
+
+        private String getAccountType (Account account){
+            String accountType = null;
+            if (account instanceof BusinessAccount)
+                return accountType = "Business Account";
+            else
+                return accountType = "Private Account";
+        }
+
+
+    }
 
