f326e016a9344c28e4867fd543fbd0e07cbb95e0
Ymke <ymkedevink@gmail.com>
2020-06-05 13:53:32 +0200

    Test for HeadBusinessController expanded

--- src/test/java/royalstacks/app/controller/HeadBusinessControllerTest.java
+++ src/test/java/royalstacks/app/controller/HeadBusinessControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import royalstacks.app.model.Employee;
+import royalstacks.app.service.BusinessAccountService;
 import royalstacks.app.service.CustomerService;
 import royalstacks.app.service.EmployeeService;
 
@@ -30,6 +31,9 @@ class HeadBusinessControllerTest {
     @MockBean
     private EmployeeService employeeService;
 
+    @MockBean
+    private BusinessAccountService businessAccountService;
+
     @Test
     void overviewHandlerTestThatItWorks() throws Exception {
         //ARRANGE
@@ -41,6 +45,7 @@ class HeadBusinessControllerTest {
         mvc.perform(
                 get("/headbusiness").sessionAttr("userid", userId)
         ).andExpect(status().isOk())
-                .andExpect(model().attributeExists("top10BusinessAccounts"));
+                .andExpect(model().attributeExists("top10BusinessAccounts"))
+                .andExpect(model().attributeExists("sectorAndTotalBalances"));
     }
 }
