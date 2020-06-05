beae54f3ee79ba612483564f8de31eabeec6d16d
Timothee <timotheebusch@hotmail.com>
2020-06-05 16:09:26 +0200

    transactionrepositorytest gelukt, acountdetail html iets beter gemaakt

--- src/test/java/royalstacks/app/model/repository/TransactionRepositoryTest.java
+++ src/test/java/royalstacks/app/model/repository/TransactionRepositoryTest.java
@@ -3,64 +3,67 @@ package royalstacks.app.model.repository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import royalstacks.app.model.Transaction;
-import static org.assertj.core.api.Assertions.*;
 
+import royalstacks.app.model.Transaction;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class TransactionRepositoryTest {
-    public static final int FROM_ACCOUNT_ID = 1;
-    @Autowired
-    private TestEntityManager testEntityManager;
+/*@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)*/
+@SpringBootTest
+class TransactionRepositoryTest {
+
+
+
 
     @Autowired
     private TransactionRepository transactionRepository;
 
 
+
     @Test
-    public void getTransactionsByFromAccountIdOrToAccountIdOrderByDateDesc() {
+    public void getTransaction() {
 
         //GIVEN
+        //TEST IF WE CAN GET A TRANSACTION FROM MONGO DB, TEST if from accountDB is what we expect
         //Create three transcation Object
-       Transaction transaction1 = new Transaction();
-        transaction1.setFromAccountId(FROM_ACCOUNT_ID);
+        Transaction transaction1 = new Transaction(5,6,300,"HELOOO", null);
+        transaction1.setTransactionId("200");
+        //SAVE IT INTO DB
 
-        Transaction transaction2 = new Transaction();
-        Transaction transaction3 = new Transaction();
-        testEntityManager.persist(transaction1);
-        testEntityManager.flush();
-
-                //get those transaction and put it in a list
-
+        transactionRepository.save(transaction1);
         //WHEN
 
-        //for now get one transaction
-        List<Transaction> transactionFound = transactionRepository.getTransactionsByFromAccountIdOrToAccountIdOrderByDateDesc(FROM_ACCOUNT_ID,FROM_ACCOUNT_ID);
-        //check the size -----> put the value of the size int an int variable
-        int listsize = transactionFound.size();
-        //give an int variable expected
+        //HAAL HET OP
+        Optional<Transaction> transactionFound = transactionRepository.findById("200");
+
+
+        //GET THE FROM_ACOUNTID
+        int fromAccountId = 0;
+        if(transactionFound.isPresent()){
+           fromAccountId = transactionFound.get().getFromAccountId();
+        }
 
         //THEN
-        // result of de test: get the all transaction from a specific account
-        // how to check: check the size and compare the actual size of your transaction list
+
+        //EXPECTED FromACCOUNTID
+        var expected = 5;
+
+        assertThat(fromAccountId).isEqualTo(expected);
         // even better check: check also if they come sorted by date
-        var expected = 1;
-        assertThat(listsize).isEqualTo(expected);
-
-
 
     }
 }
