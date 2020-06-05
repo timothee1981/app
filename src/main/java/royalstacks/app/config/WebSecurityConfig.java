d5e2381681f3fb8d6b0894d583e13a3f477f1132
Stefan van Horssen <sv_horssen@hotmail.com>
2020-06-05 17:51:35 +0200

    added webjar to use Jquery in my html-page (myacounts.html)

--- src/main/java/royalstacks/app/config/WebSecurityConfig.java
+++ src/main/java/royalstacks/app/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package royalstacks.app.config;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,10 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig
     extends WebSecurityConfigurerAdapter implements ApplicationContextAware{
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    // ...
-                    .csrf().disable();
-        }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/webjars/**").permitAll();
+
+    }
 }
+
