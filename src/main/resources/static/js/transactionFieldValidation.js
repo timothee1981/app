65dab8b4b3d8b2dfd11d01c2e2339d41b316e498
Tom Janssen <t.janssen13788@gmail.com>
2020-06-06 21:46:30 +0200

    javascript: client-side checks, alleen nog met database checken

--- src/main/resources/static/js/transactionFieldValidation.js
+++ src/main/resources/static/js/transactionFieldValidation.js
@@ -4,68 +4,136 @@ const amountFieldId = "amount";
 const descriptionFieldId = "description";
 const toAccountErrorId = "toAccountError";
 const amountErrorId = "amountError";
-
-
 const fromAccountField = document.getElementById(fromAccountFieldId);
 const toAccountField = document.getElementById(toAccountFieldId);
 const amountField = document.getElementById(amountFieldId);
-
-
-const errorAccountDoesntExist = "Enter an existing Royal Stacks account number <br>"
+const errorAccountDoesntExist = "Enter an existing Royal Stacks account number"
 const errorSenderSameAsReceiver = "The receiving account can't be the same as the sending account"
 const errorAmountTooHigh = "The amount can't be higher than the available balance"
 const errorAmountFormatInvalid = "The amount should only consist of numbers, with a maximum of two decimals"
+const errorNoAmount = "Enter the amount to be transfered"
 
 
-fromAccountField.addEventListener("click",function () {
-    if(amountField.value.length > 0){
-        if(isAmountValid()){
-            addClassValidRemoveClassInvalid(amountFieldId);
-        }
-        else {
-            addClassInvalidRemoveClassValid(amountFieldId);
-        }
+fromAccountField.addEventListener("click",fromAccountValidationAction);
+toAccountField.addEventListener("input", function(){
+    if(toAccountField.value.length > 17) {
+        toAccountValidationAction();
+    }
+    else{
+        removeValidClass(toAccountFieldId)
+    }
+});
+toAccountField.addEventListener("blur", toAccountValidationAction);
+amountField.addEventListener("input", amountValidationAction);
+amountField.addEventListener("blur", function pleaseEnterAmount(){
+    if(amountField.value.length == 0 ){
+        showError(errorNoAmount, amountErrorId);
+        setClassInvalid(amountFieldId);
     }
 });
 
-toAccountField.addEventListener("input",function() {
-     if (isToAccountValid()) {
-           addClassValidRemoveClassInvalid(toAccountFieldId);
-     }
-     else {
-            addClassInvalidRemoveClassValid(toAccountFieldId);
-     }
-} );
 
-amountField.addEventListener("input", function () {
-    if (isAmountValid()) {
-        addClassValidRemoveClassInvalid(amountFieldId);
+
+function fromAccountValidationAction() {
+    if(amountField.value.length > 0) {
+        showHideAmountValidMark();
+        showHideAmountErrors();
+    }
+}
+function toAccountValidationAction(){
+    showHideToAccountErrors();
+    showHideToAccountValidMark();
+}
+function amountValidationAction() {
+    showHideAmountErrors();
+    showHideAmountValidMark();
+}
+
+
+function showHideToAccountErrors(){
+
+    if(isAccountFormatValid()) {
+        hideError(errorAccountDoesntExist, toAccountErrorId);
+     }
+    else{
+        showError(errorAccountDoesntExist, toAccountErrorId);
+    }
+    if(isAccountDifferentThanFromAccount()){
+        hideError(errorSenderSameAsReceiver, toAccountErrorId);
     }
     else{
-        addClassInvalidRemoveClassValid(amountFieldId);
+        showError(errorSenderSameAsReceiver, toAccountErrorId);
     }
-})
+}
+function showHideAmountErrors(){
+    hideError(errorNoAmount, amountErrorId);
+    if(isAmountFormatValid()){
+        hideError(errorAmountFormatInvalid, amountErrorId);
+    }
+    else{
+        showError(errorAmountFormatInvalid, amountErrorId);
+    }
+    if(isAmountSmallerThanBalance()){
+        hideError(errorAmountTooHigh, amountErrorId);
+    }
+    else {
+        showError(errorAmountTooHigh, amountErrorId);
+    }
+}
+function showHideAmountValidMark(){
+    if(isAmountValid()){
+        setClassValid(amountFieldId);}
+    else{
+        setClassInvalid(amountFieldId);
+    }
+}
+
+function showHideToAccountValidMark(){
+    if(isToAccountValid()){
+        setClassValid(toAccountFieldId);}
+    else{
+        setClassInvalid(toAccountFieldId);
+    }
+}
 
 
 
 
 function showError(errorMessage, elementId){
-    if(document.getElementById(elementId).innerHTML.indexOf(errorMessage) === -1) {
-        document.getElementById(elementId).innerHTML = errorMessage;
+    let currentText = document.getElementById(elementId).innerHTML;
+    if(!currentText.includes(errorMessage)) {
+        let newString = currentText.concat(errorMessage);
+        document.getElementById(elementId).innerHTML = newString;
     }
 }
 function hideError(errorMessage, elementId){
-    if(document.getElementById(elementId).innerHTML.indexOf(errorMessage) !== -1) {
-    document.getElementById(elementId).innerHTML = "";
+    let currentText = document.getElementById(elementId).innerHTML;
+    if(currentText.includes(errorMessage)) {
+        let newString = currentText.replace(errorMessage, "");
+        document.getElementById(elementId).innerHTML = newString;
     }
 }
-function addClassValidRemoveClassInvalid(id) {
-   document.getElementById(id).classList.add('isValid');
-    document.getElementById(id).classList.remove('isInvalid');
+function setClassValid(id) {
+    if(document.getElementById(id).classList.contains('isInvalid')){
+        document.getElementById(id).classList.remove('isInvalid');
+    }
+    if(!document.getElementById(id).classList.contains('isValid')) {
+        document.getElementById(id).classList.add('isValid');
+    }
+
 }
-function addClassInvalidRemoveClassValid(id) {
-    document.getElementById(id).classList.add('isInvalid');
-    document.getElementById(id).classList.remove('isValid');
+function setClassInvalid(id) {
+    if(!document.getElementById(id).classList.contains('isInvalid')) {
+        document.getElementById(id).classList.add('isInvalid');
+    }
+    if(document.getElementById(id).classList.contains('isValid')){
+        document.getElementById(id).classList.remove('isValid');
+    }
+}
+function removeValidClass(id){
+    if(document.getElementById(id).classList.contains('isValid')){
+        document.getElementById(id).classList.remove('isValid');
+    }
 }
 
 
@@ -89,51 +157,23 @@ function isDescriptionValid(){
     return
 }
 
+
 function isAccountFormatValid(){
     let accountFormatRegex = new RegExp(/^nl[\d]{2}roya[\d]{10}$/i);
-    if (accountFormatRegex.test(toAccountField.value)){
-        hideError(errorAccountDoesntExist, toAccountErrorId);
-        return true;
-    }
-    else{
-        showError(errorAccountDoesntExist, toAccountErrorId);
-        return false;
-    }
+    return (accountFormatRegex.test(toAccountField.value));
 }
 function isAccountDifferentThanFromAccount(){
-    if (fromAccountField.value.toUpperCase() !== toAccountField.value.toUpperCase()){
-        hideError(errorSenderSameAsReceiver, toAccountErrorId);
-        return true;
-    }
-    else {
-        showError(errorSenderSameAsReceiver, toAccountErrorId);
-        return false;
-    }
+    return(fromAccountField.value.toUpperCase() !== toAccountField.value.toUpperCase());
 }
 function doesAccountExistInDb(){
     return true;
 }
-function  isAmountFormatValid(){
-    let amountFormatRegex = new RegExp(/^\d+([,.][\d]{1,2})?$/);
-    if(amountFormatRegex.test(amountField.value)){
-        hideError(errorAmountFormatInvalid, amountErrorId);
-        return true;
-    }
-    else{
-        showError(errorAmountFormatInvalid, amountErrorId);
-        return false;
-    }
-
+function isAmountFormatValid(){
+    let amountFormatRegex = new RegExp(/^\d{0,11}([,.][\d]{1,2})?$/);
+    return (amountFormatRegex.test(amountField.value));
 }
 function isAmountSmallerThanBalance(){
-    if(parseInt(fromAccountField.options[fromAccountField.selectedIndex].text, 10) >= amountField.value){
-        hideError(errorAmountTooHigh, amountErrorId);
-        return true;
-    }
-    else{
-        showError(errorAmountTooHigh, amountErrorId);
-        return false;
-    }
+    return (parseInt(fromAccountField.options[fromAccountField.selectedIndex].text, 10) >= amountField.value);
 }
 
 
