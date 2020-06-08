6a9c5310d90543185cab7cdf9c8836749c782f4d
Suzanne Knauff <suzanneknauff@gmail.com>
2020-06-08 20:29:45 +0200

    dode code weggehaald

--- src/main/java/royalstacks/app/service/AccountHolderInviteService.java
+++ src/main/java/royalstacks/app/service/AccountHolderInviteService.java
@@ -28,18 +28,5 @@ public class AccountHolderInviteService {
         return verificationCode.matches("\\d{5}");
     }
 
-/*    public boolean isInviteNew(){
-MYSQL Query: SELECT * FROM account_holder_invite WHERE account_account_id = 2 AND invitee_userid = 4;
-        select * from accountholderinvitation where account_account_id = account.getAccountId AND invitee_userid = user.getUserId;
-        if (result == null)
-        return true;
-        update verification_code with value verificationCode;
-    }*/
-
-/*    public void overwriteInvite(String verificationCode){
-        if (!isInviteNew){
-            UPDATE account_holder_invite SET verification_code = verificationCode WHERE account_account_id = 2 AND invitee_userid = 4;
-        }
-    }*/
 
 }
